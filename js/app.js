b1cac848b68a2642d08d8365ade716e12a1ceac4
Nelson Juarez Rivas <najr91@gmail.com>
2023-06-06 16:32:37 -0300

    se agrego la logica de los botones que controlan el reloj

--- js/app.js
+++ js/app.js
@@ -45,5 +45,20 @@ function obtenerFecha(){
 
 }
 
+function cambiarColor(color){
+    let section = document.getElementById("reloj");
+    if(color === 'rosa'){
+    section.className = "w-75 pink"; 
+    }
+    if(color === 'verde'){
+        section.className = "w-75 green"; 
+        }
+    if(color === 'azul'){
+            section.className = "w-75 blue"; 
+            }
+}
+
+
+
 window.setInterval(obtenerFecha,1000);
 
